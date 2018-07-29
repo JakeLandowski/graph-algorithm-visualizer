07eb661cfd7ed5fa5d3fa6ac71b011e1bbe9db91
rmarlowg <rmarlow@mail.greenriver.edu>
2018-07-28 18:36:42 -0700

    Animation intro

--- scripts/ui-animations/animations.js
+++ scripts/ui-animations/animations.js
@@ -3,7 +3,21 @@ define(['ui-animations/anime'], function(Anime)
     return {
         start()
         {
+            function generateUI(component,direction,width,height,delay) {
+                let enterUI = Anime ({
+                    targets: component,
+                    width: width,
+                    height: height,
+                    translateX: direction,
+                    easing: 'easeInOutExpo',
+                    delay: delay
+                })
+            }
 
+            let createNew = document.querySelector('#createnewbtn');
+            let hideUI = document.querySelector('#hide-ui');
+            generateUI(hideUI,200,200,25,250);
+            generateUI(createNew,200,200,50,350);
         }
     };
 });
