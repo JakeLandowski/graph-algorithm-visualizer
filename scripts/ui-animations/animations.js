514cce5cba0c4900380ae9eac508ef11612fdf19
rmarlowg <rmarlow@mail.greenriver.edu>
2018-07-28 17:32:35 -0700

    Anime.js working example

--- scripts/ui-animations/animations.js
+++ scripts/ui-animations/animations.js
@@ -6,13 +6,13 @@ define(['ui-animations/anime'], function(Anime)
             let pathEls = document.querySelectorAll('path');
             for (let i = 0; i < pathEls.length; i++) {
                 let pathEl = pathEls[i];
-                let offset = anime.setDashoffset(pathEl);
+                let offset = Anime.setDashoffset(pathEl);
                 pathEl.setAttribute('stroke-dashoffset', offset);
-                anime({
+                Anime({
                     targets: pathEl,
                     strokeDashoffset: [offset, 0],
-                    duration: anime.random(1000, 3000),
-                    delay: anime.random(0, 2000),
+                    duration: Anime.random(1000, 3000),
+                    delay: Anime.random(0, 2000),
                     loop: true,
                     direction: 'alternate',
                     easing: 'easeInOutSine',
