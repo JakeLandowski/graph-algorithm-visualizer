f087df34cd1e3574a45417c990f90c646dae3062
rmarlowg <rmarlow@mail.greenriver.edu>
2018-07-28 17:36:51 -0700

    Removed SVG overlay

--- scripts/ui-animations/animations.js
+++ scripts/ui-animations/animations.js
@@ -3,22 +3,7 @@ define(['ui-animations/anime'], function(Anime)
     return {
         start()
         {
-            let pathEls = document.querySelectorAll('path');
-            for (let i = 0; i < pathEls.length; i++) {
-                let pathEl = pathEls[i];
-                let offset = Anime.setDashoffset(pathEl);
-                pathEl.setAttribute('stroke-dashoffset', offset);
-                Anime({
-                    targets: pathEl,
-                    strokeDashoffset: [offset, 0],
-                    duration: Anime.random(1000, 3000),
-                    delay: Anime.random(0, 2000),
-                    loop: true,
-                    direction: 'alternate',
-                    easing: 'easeInOutSine',
-                    autoplay: true
-                });
-            }
+
         }
     };
 });
