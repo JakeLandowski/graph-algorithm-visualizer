ebd93cb659923885ec168af70210f342adf5257c
rmarlowg <rmarlow@mail.greenriver.edu>
2018-07-28 17:25:55 -0700

    Added anime.js library

--- scripts/ui-animations/animations.js
+++ scripts/ui-animations/animations.js
@@ -3,7 +3,22 @@ define(['anime'], function(Anime)
     return {
         start()
         {
-
+            let pathEls = document.querySelectorAll('path');
+            for (let i = 0; i < pathEls.length; i++) {
+                let pathEl = pathEls[i];
+                let offset = anime.setDashoffset(pathEl);
+                pathEl.setAttribute('stroke-dashoffset', offset);
+                anime({
+                    targets: pathEl,
+                    strokeDashoffset: [offset, 0],
+                    duration: anime.random(1000, 3000),
+                    delay: anime.random(0, 2000),
+                    loop: true,
+                    direction: 'alternate',
+                    easing: 'easeInOutSine',
+                    autoplay: true
+                });
+            }
         }
     };
 });
