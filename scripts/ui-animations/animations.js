05018c1426f20691384ed189ba4b32ea7574b7de
marlowr <mr.ryanmarlow@gmail.com>
2018-07-31 13:19:53 -0700

    Added animated line for UI

--- scripts/ui-animations/animations.js
+++ scripts/ui-animations/animations.js
@@ -3,6 +3,29 @@ define(['ui-animations/anime'], function(Anime)
     return {
         start()
         {
+            //Sets specific buttons as variables, set default delay at 0.
+            let createNew = document.querySelector('#createnewbtn');
+            let hideUI = document.querySelector('#hide-ui');
+            let hideTools = document.querySelector('#hide-tools');
+            let tools = document.querySelectorAll('.toolsbtn');
+            let delay = 0;
+            let openTools = true;
+
+            generateUI(hideUI,200,200,25,0);
+            generateUI(createNew,200,200,50,100);
+            drawLine('alternate',1000);
+
+            function drawLine(direction,duration) {
+                let lineDrawing = Anime({
+                    targets: '#lineDrawing .lines path',
+                    strokeDashoffset: [Anime.setDashoffset, 0],
+                    easing: 'easeInOutSine',
+                    duration: duration,
+                    delay: function(el, i) { return i * 250 },
+                    direction: direction,
+                    loop: false
+                });
+            }
 
             function generateUI(component,direction,width,height,delay) {
                 let enterUI = Anime ({
@@ -15,20 +38,13 @@ define(['ui-animations/anime'], function(Anime)
                 })
             }
 
-            let createNew = document.querySelector('#createnewbtn');
-            let hideUI = document.querySelector('#hide-ui');
-            let hideTools = document.querySelector('#hide-tools');
-            let tools = document.querySelectorAll('.toolsbtn');
 
-            let delay = 0;
             tools.forEach(function (element) {
                 generateUI(element,-240,200,25,delay);
                 delay = delay + 100;
             });
-            generateUI(hideUI,200,200,25,0);
-            generateUI(createNew,200,200,50,100);
 
-            let openTools = true;
+
             hideTools.addEventListener('click',(function() {
                 delay = 0;
                 if(openTools) {
@@ -47,26 +63,25 @@ define(['ui-animations/anime'], function(Anime)
 
             }));
 
+
             hideUI.addEventListener('click', (function () {
                 let text = document.getElementById("hide");
                 if(text.innerHTML === "Hide UI") {
                     generateUI(createNew,-200,0,0,0);
                     text.innerHTML = "Show UI";
+                    let lineDrawing = Anime({
+                        targets: '#lineDrawing .lines path',
+                        strokeDashoffset: [Anime.setDashoffset, 0],
+                        easing: 'easeInOutSine',
+                        duration: 500,
+                        direction: 'reverse',
+                    });
                 } else {
                     generateUI(createNew,200,200,50,0);
                     text.innerHTML = "Hide UI";
+                    drawLine('alternate',1000);
                 }
             }));
-
-            let lineDrawing = Anime({
-                targets: '#lineDrawing .lines path',
-                strokeDashoffset: [Anime.setDashoffset, 0],
-                easing: 'easeInOutSine',
-                duration: 1500,
-                delay: function(el, i) { return i * 250 },
-                direction: 'alternate',
-                loop: true
-            });
         }
     };
 });
